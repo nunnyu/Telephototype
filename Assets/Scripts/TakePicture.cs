4b64dada444bc8bd0f449c8c6b3603fd0d030cfd
Anthony Basko <antony.basko@gmail.com>
2025-07-12 19:30:06 +0900

    Camera Sprites & Functionality
    up down left right appear when you press space! will animate them next.

--- Assets/Scripts/TakePicture.cs
+++ Assets/Scripts/TakePicture.cs
@@ -5,21 +5,35 @@ public class TakePicture : MonoBehaviour
 {
     [Header("This script should be attached to an empty parent.")]
     [SerializeField] private GameObject defaultPose;
-    [SerializeField] private GameObject cameraPose;
+    [SerializeField] private GameObject cameraPoseUp;
+    [SerializeField] private GameObject cameraPoseDown;
+    [SerializeField] private GameObject cameraPoseLeft;
+    [SerializeField] private GameObject cameraPoseRight;
+
+    private GameObject cameraPose;
     private Vector2 lastPosition;
-    private Boolean takingPic;
+    private bool takingPic;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         defaultPose.SetActive(true);
+        cameraPoseUp.SetActive(false);
+        cameraPoseDown.SetActive(false);
+        cameraPoseLeft.SetActive(false);
+        cameraPoseRight.SetActive(false);
+
+        cameraPose = cameraPoseDown; // Just a default, incase there are any weird bugs
         cameraPose.SetActive(false);
+
         takingPic = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ChooseCameraPose();
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (!takingPic)
@@ -27,6 +41,26 @@ public class TakePicture : MonoBehaviour
                 OnCameraInput();
                 takingPic = true;
             }
+        }
+    }
+
+    void ChooseCameraPose()
+    {
+        if (PlayerMovement.MovingUp)
+        {
+            cameraPose = cameraPoseUp;
+        }
+        else if (PlayerMovement.MovingDown)
+        {
+            cameraPose = cameraPoseDown;
+        }
+        else if (PlayerMovement.MovingLeft)
+        {
+            cameraPose = cameraPoseLeft;
+        }
+        else if (PlayerMovement.MovingRight)
+        {
+            cameraPose = cameraPoseRight;
         }
     }
 
